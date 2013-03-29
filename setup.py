fcf4ddecab6ef645045c50a8723de8e0697bce62
Andres Riancho <andres.riancho@gmail.com>
2013-03-29 16:37:01 -0300

    Correct module name

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
       
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['python-daemon', 'psutils'],
+      install_requires=['python-daemon', 'psutil'],
       
       entry_points={
           'console_scripts':
